04fe6871218e90d883b45a773456ed8b1a97d57a
Sxpxii <supanida25430812@gmail.com>
2025-02-27 14:50:31 +0700

    28

--- frontend/src/configAPI.js
+++ frontend/src/configAPI.js
@@ -1,8 +1,9 @@
 // src/config.js
 const config = {
-    API_URL: "http://192.168.5.93:3002" //L5
+    //API_URL: "http://192.168.5.93:3002" //L5
     //API_URL: "http://192.168.1.177:3002" //หอ
     //API_URL: "http://172.20.10.6:3002" 
+    API_URL:"http://localhost:3002"
     
 };
 

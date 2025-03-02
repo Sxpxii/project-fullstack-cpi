e716c8a0c240b9b176fc1705f163727598ab93b7
Sxpxii <supanida25430812@gmail.com>
2025-03-03 00:11:08 +0700

    30

--- frontend/src/configAPI.js
+++ frontend/src/configAPI.js
@@ -3,7 +3,7 @@ const config = {
     //API_URL: "http://192.168.5.93:3002" //L5
     //API_URL: "http://192.168.1.177:3002" //หอ
     //API_URL: "http://172.20.10.6:3002" 
-    API_URL: "http://mywebapp-backend-container:3002"
+    API_URL: "http://backend:3002"
     
 };
 
